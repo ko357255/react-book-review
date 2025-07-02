ada5206534cb42aa412f2e3b4f747d668f16bdbb
ko357255 <mikanace@outlook.jp>
2025-07-02 10:43:41 +0900

    vitesをインストール

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { configDefaults } from 'vitest/config' // vitest
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTest.ts',
+  }
 });
