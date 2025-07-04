fb4b4f89959a2b2ac2cf6e0646347b8eee54e4b2
ko357255 <mikanace@outlook.jp>
2025-07-05 00:10:46 +0900

    認証トークンに基づいてルーティングを制御する機能を追加

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Sidebar from '@/components/Sidebar';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import NotFound from '@/pages/NotFound';
 import SignUp from '@/pages/SignUp';
 import './index.css';
+import { useEffect, useState } from 'react';
 
 const Router = () => {
+  const [auth, setAuth] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    setAuth(!!token);
+  }, []);
+
   return (
     <BrowserRouter>
       {/* サイドバー(左) */}
@@ -19,9 +27,17 @@ const Router = () => {
         }}
       >
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+
+          {auth ? (
+            // ログイン中
+            <Route path="/" element={<Home />} />
+          ) : (
+            // <Navigate> 描写すると遷移する
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          )}
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
