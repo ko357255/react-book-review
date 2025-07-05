ca395762139f0bee8e9489f0052f0c8f20a1eb04
ko357255 <mikanace@outlook.jp>
2025-07-05 22:07:18 +0900

    stateを表示させる

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import TestItem from '@/components/TestItem';
+import type { RootState } from '@/store';
+import { add, remove, set } from '@/store/test';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 const Home = () => {
-  return <div>
-    <h2>ホーム</h2>
-  </div>;
+  // testの値
+  const value = useSelector((state: RootState) => state.test.value);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="home">
+      <h2>ホーム</h2>
+      <p>test: {value}</p>
+      <button onClick={() => dispatch(add())}>add</button>
+      <button onClick={() => dispatch(remove())}>remove</button>
+      <button onClick={() => dispatch(set(0))}>set</button>
+      <TestItem />
+    </div>
+  );
 };
 
 export default Home;
