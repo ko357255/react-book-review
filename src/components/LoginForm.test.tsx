2ea8462d7f324b54623dd04a8b7a463463d864ee
ko357255 <mikanace@outlook.jp>
2025-07-07 20:04:00 +0900

    @types/nodeとundici-typesを追加し、tsconfigのbaseUrlとpathsを修正

--- src/components/LoginForm.test.tsx
+++ src/components/LoginForm.test.tsx
@@ -3,22 +3,51 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import LoginForm from './LoginForm';
+import { MemoryRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '@/store/auth';
+import { Provider } from 'react-redux';
+
+// テスト用のstore
+const store = configureStore({
+  reducer: {
+    auth: authSlice.reducer,
+  },
+});
 
 describe('ログインフォーム', () => {
   it('メールアドレスの入力欄が表示される', () => {
-    render(<LoginForm />); // ログインフォームを描画
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginForm />
+        </MemoryRouter>
+      </Provider>,
+    ); // ログインフォームを描画
     // .getByLabelText(): ラベルのコントロール(input)を取得
     // .toBeInTheDocument(): DOMが存在しているか
     expect(screen.getByLabelText('メールアドレス')).toBeInTheDocument();
   });
 
   it('パスワードの入力欄が表示される', () => {
-    render(<LoginForm />);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginForm />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(screen.getByLabelText('パスワード')).toBeInTheDocument();
   });
 
   it('ログインボタンが表示される', () => {
-    render(<LoginForm />);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <LoginForm />
+        </MemoryRouter>
+      </Provider>,
+    );
     expect(
       screen.getByRole('button', { name: 'ログイン' }),
     ).toBeInTheDocument();
