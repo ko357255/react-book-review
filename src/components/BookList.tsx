08e36d5cefde22108833fcacf9b14cfcb0969d10
ko357255 <mikanace@outlook.jp>
2025-07-07 17:54:17 +0900

    useQueryでキャッシュ化

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,42 +1,24 @@
-import { useEffect, useState } from 'react';
-import { bookGet, type BookData } from '@/api/book';
+// import { useEffect, useState } from 'react';
+import { bookGet } from '@/api/book';
 import { Spinner } from 'react-bootstrap';
 import BookItem from '@/components/BookItem';
 import { useQuery } from '@tanstack/react-query';
 
 const BookList = ({ offset }: { offset: number }) => {
-  // const [books, setBooks] = useState<BookData[]>([]);
-  // const [error, setError] = useState<string | null>(null);
-  // const [isLoading, setIsLoading] = useState(false);
 
-  const { data: books, isLoading, isError, error } = useQuery({
+  const {
+    data: books,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    // 異なるキャッシュかどうかを判別するための値
     queryKey: ['books', offset],
+    // データを渡す関数
     queryFn: () => bookGet(offset),
-    staleTime: 1000 * 60 * 1,
+    // キャッシュの有効期限
+    staleTime: 1000 * 30,
   });
-
-
-  // useEffect(() => {
-  //   const fetchBooks = async () => {
-  //     setError(null);
-  //     setIsLoading(true);
-
-  //     try {
-  //       const books = await bookGet(offset);
-  //       setBooks(books);
-  //     } catch (e) {
-  //       if (e instanceof Error) {
-  //         setError(e.message);
-  //       } else {
-  //         setError('予期せぬエラーが発生しました');
-  //       }
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
-
-  //   fetchBooks();
-  // }, [offset]);
 
   return (
     <div className="book-list">
@@ -46,9 +28,7 @@ const BookList = ({ offset }: { offset: number }) => {
         </Spinner>
       )}
       {isError && <p className="text-danger">{error.message}</p>}
-      {books && books.map((book) => (
-        <BookItem key={book.id} book={book} />
-      ))}
+      {books && books.map((book) => <BookItem key={book.id} book={book} />)}
     </div>
   );
 };
