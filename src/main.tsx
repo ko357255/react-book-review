2ca1d25fd6f4b0090e97f5195be660ebb064cc0f
ko357255 <mikanace@outlook.jp>
2025-07-08 17:11:25 +0900

    storeのパスの修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { store } from '@/store/index.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App.tsx';
-import { store } from './store/index.ts';
 
 const queryClient = new QueryClient();
 
