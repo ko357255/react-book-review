c06416ba5d3e9d61eb6e32605ae05d300e225dec
ko357255 <mikanace@outlook.jp>
2025-07-09 02:28:37 +0900

    レビュー作成APIを追加

--- src/api/book.ts
+++ src/api/book.ts
@@ -11,18 +11,43 @@ export interface BookData {
   isMine: boolean;
 }
 
-export const bookGet = async (offset: number = 0): Promise<BookData[]> => {
-  const token = localStorage.getItem('authToken');
+interface BookCreateRequest {
+  title: string;
+  url: string;
+  detail: string;
+  review: string;
+}
 
+interface BookCreateResponse {
+  id: string;
+  title: string;
+  url: string;
+  detail: string;
+  review: string;
+  reviewer: string;
+  isMine: boolean;
+}
+
+export const bookGet = async (offset: number = 0): Promise<BookData[]> => {
   try {
     const response = await axiosInstance.get('/books', {
       params: {
         offset: offset,
       },
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
     });
+
+    return response.data;
+  } catch (e: unknown) {
+    // エラーを整形して発生させる
+    return handleError(e);
+  }
+};
+
+export const bookCreate = async (
+  bookData: BookCreateRequest,
+): Promise<BookCreateResponse> => {
+  try {
+    const response = await axiosInstance.post('/books', bookData);
 
     return response.data;
   } catch (e: unknown) {
